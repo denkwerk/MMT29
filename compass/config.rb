3c5a751e6d7ebdcc4b2d7d5671c99d5c28eac470
andre.laugks@elevado.de <alaugks@mac50.mac.denkwerk.int>
2012-03-26 11:42:36 +0200

    Vortrag hinzugefügt

---
+++ compass/config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+#relative_assets = false
+
+output_style = (environment == :production) ? :compressed : :expanded
+
+# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
+
